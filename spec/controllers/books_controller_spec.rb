ab69641ce7b290189bdbc51649efd8594e9a3a75
Ian Bui <thinkbui@gmail.com>
2020-03-05 16:43:32 -0600

    Added Book show route

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -5,4 +5,13 @@ RSpec.describe BooksController, type: :controller do
       expect(response).to be_successful
     end
   end
+
+  describe "GET #show" do
+    it "returns a success response" do
+      book = Book.new(name:"Test")
+      book.save!
+      get :show, params: {id: book.id}
+      expect(response).to be_successful
+    end
+  end
 end
