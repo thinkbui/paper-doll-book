1771cee19cb2b12137f1411786416943270b5ad4
Ian Bui <thinkbui@gmail.com>
2020-03-19 01:37:21 -0500

    Added BookPage index endpoint

---
+++ app/controllers/book_pages_controller.rb
@@ -0,0 +1,41 @@
+class BookPagesController < ApplicationController
+  def index
+    @book = Book.find(params[:book_id])
+    @book_pages = @book.book_pages
+  end
+
+  # def new
+  #   @book = Book.new
+  # end
+
+  # def show
+  #   @book = Book.find(params[:id])
+  # end
+
+  # def edit
+  #   @book = Book.find(params[:id])
+  # end
+
+  # def create
+  #   book = Book.new
+  #   book.name = params[:book][:name]
+  #   book.description = params[:book][:description]
+  #   book.save!
+  #   book.reload
+  #   redirect_to action: "show", id: book.id
+  # end
+
+  # def update
+  #   @book = Book.find(params[:id])
+  #   @book.name = params[:book][:name]
+  #   @book.description = params[:book][:description]
+  #   @book.save!
+  #   redirect_to action: "show", id: @book.id
+  # end
+
+  # def destroy
+  #   @book = Book.find(params[:id])
+  #   @book.destroy!
+  #   redirect_to action: "index"
+  # end
+end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,5 @@ class Book < ApplicationRecord
 	validates :name, :presence => true
 
 	has_many :pages
+	has_many :book_pages
 end
